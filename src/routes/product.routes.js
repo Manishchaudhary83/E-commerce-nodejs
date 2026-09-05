fa9351a4ed8abed19966419953fc3de61cd87f44
Manish Chaudhary <manishhjaiswal095@gmail.com>
2026-09-05 21:20:47 +0545

    add api of update and delete

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,13 +1,16 @@
 const express = require("express")
-const {createProductController, getAllProductsController} = require("../controllers/product.controller")
+const {createProductController, getAllProductsController, updateProductController, deleteProductController} = require("../controllers/product.controller")
 const authMiddleware = require("../middlewares/auth.middleware")
+const upload = require("../middlewares/multer.middleware")
 
 
 const router = express.Router()
 
 
-router.post("/create", authMiddleware, createProductController)
+router.post("/create", authMiddleware, upload.array("images", 5), createProductController)
 router.get('/', authMiddleware, getAllProductsController)
+router.patch('/update/:id', authMiddleware, updateProductController)
+router.delete('/delete/:id', authMiddleware, deleteProductController)
 
 
 module.exports = router
