84f3f4b151735ae75e9516f83a43838bf1877a61
Manish Chaudhary <manishhjaiswal095@gmail.com>
2026-09-05 21:19:37 +0545

    update and delete in product controllers

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,4 +1,4 @@
-const { createproduct, getAllproducts } = require("../services/product.service")
+const { createproduct, getAllproducts, updateProduct, deleteProduct } = require("../services/product.service")
 
 module.exports = {
     createProductController: async (req, res, next) => {
@@ -34,5 +34,53 @@ getAllProductsController: async (req, res, next) => {
             } catch (error) { 
                 return next(error); 
             }
-        }
+        },
+//update products
+updateProductController: async (req, res, next) => {
+    try {
+        const userId = req.user.userId;
+
+        const { id } = req.params;
+        const data = req.body;
+
+        const product = await updateProduct(
+            id,
+            userId,
+            data
+        );
+
+        return res.status(product.status).json({
+            message: product.message,
+            product: product.product
+        });
+
+    } catch (error) {
+        return next(error);
+    }
+},
+
+
+//delete products
+deleteProductController: async (req, res, next) => {
+    try {
+
+        const userId = req.user.userId;
+        const { id } = req.params;
+
+        const product = await deleteProduct(
+            id,
+            userId
+        );
+
+        return res.status(product.status).json({
+            message: product.message
+        });
+
+    } catch (error) {
+        return next(error);
+    }
+}
+
+
+
 }
