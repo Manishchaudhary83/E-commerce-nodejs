ec0715e715f1427d8563bb6209b23c115a5a0df1
Manish Chaudhary <manishhjaiswal095@gmail.com>
2026-09-05 14:42:31 +0545

    create cart model

--- src/models/cart.model.js
+++ src/models/cart.model.js
@@ -29,5 +29,5 @@ const cartSchema = new mongoose.Schema(
         timestamps: true
     }
 );
-
-module.exports = mongoose.model("Cart", cartSchema);
+const cartModel = mongoose.model("Cart", cartSchema);
+module.exports = cartModel
