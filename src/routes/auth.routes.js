80de5cd39d50777bbc801305c8514f2e934fe916
Manish Chaudhary <manishhjaiswal095@gmail.com>
2026-09-05 21:20:05 +0545

    moidified auth controllers

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,11 @@
 const express = require("express")
 const { registerUserController, loginUserController } = require("../controllers/auth.controller")
-
+const {registerUserValidationRules} = require("../middlewares/validate.middleware")
 
 const router = express. Router()
 
 
-router.post("/register", registerUserController)
+router.post("/register", registerUserValidationRules, registerUserController)
 router.post("/login", loginUserController)
 
 module.exports = router
